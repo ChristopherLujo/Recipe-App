9ea39330ad9f8a618e2cd33d95e6ff21b2f34351
unknown <luj17001@byui.edu>
2019-12-16 23:46:12 -0700

    Finished Project

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ var express = require('express'),
     pg = require('pg'),
     app = express();
 
-//Will add this when database is created. 
-// const connect = "postgres://username:password@localhost/database";
+var connect = "postgres://Admin:cs313@localhost/recipebookdb";
 
 //Assign Dusrt Engine to .dust files
 app.engine('dust', cons.dust);
@@ -24,9 +23,57 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/', function(req, res){
-res.render("index")
+    pg.connect(connect, function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('SELECT * FROM recipes', function(err, result) {
+            if(err) {
+                return console.error('error running query', err);
+        }
+        res.render('index', {recipes: result.rows});
+        done();
+        });
+    });
 });
 
+app.post('/add', function(req, res){
+    pg.connect(connect, function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query("INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)", [req.body.name, req.body.ingredients, req.body.directions]);
+
+        done();
+        res.redirect('/')
+    });    
+});
+
+app.delete('/delete/:id', function(req, res){
+    pg.connect(connect, function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query("DELETE FROM recipes where id = $1",
+        [req.params.id]);
+
+        done();
+        res.send(200);
+    });    
+});
+
+app.post('/edit', function(req,res){
+    pg.connect(connect, function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query("UPDATE recipes set name=$1, ingredients=$2, directions=$3 WHERE id=$4",
+        [req.body.name, req.body.ingredients, req.body.directions, req.body.id]);
+
+        done();
+        res.redirect('/');
+    });    
+});
 //Server
 app.listen(3000, function(){
     console.log('Server Stated on Port 3000');
